6bea1dc605cf44675b3710584497c7979b01715b
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-03-01 14:51:54 -0800

    lucas series works, both function and test. Also got a working sum-series test and function for one trial.

--- series.py
+++ series.py
@@ -11,6 +11,21 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-def lucas(n):
+def lucas(m):
     """Return nth value in the lucas series."""
-    pass
+    if m == 1:
+        return 2
+    elif m == 2:
+        return 1
+    else:
+        return lucas(m - 1) + lucas(m - 2)
+
+
+def sum_series(p, x=0, y=1):
+    """Return nth value in some random series."""
+    if p == 1:
+        return x
+    elif p == 2:
+        return y
+    else:
+        return sum_series(p - 1) + sum_series(p - 2)
--- test_series.py
+++ test_series.py
@@ -15,7 +15,21 @@ def test_fibonacci_2():
 
 
 @pytest.mark.parametrize('n, result', [(1, 0), (2, 1), (3, 1)])
-def test_fibonacci_3(n, result):
+def test_fibonacci(n, result):
     """Test fibonacci function."""
     from series import fibonacci
     assert fibonacci(n) == result
+
+
+@pytest.mark.parametrize('m, results', [(1, 2), (2, 1), (3, 3)])
+def test_lucas(m, results):
+    """Test lucas function."""
+    from series import lucas
+    assert lucas(m) == results
+
+
+# @pytest.mark.parametrize('p, x, y', [(, x, y), ()])
+def test_sum_series():
+    """Test lucas function."""
+    from series import sum_series
+    assert sum_series(1) == 0
